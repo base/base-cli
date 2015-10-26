1774cebefe571ecade9bf9f736fe682716608d52
jonschlinkert <jon.schlinkert@sellside.com>
2015-10-26 18:42:55 -0400

    examples

---
+++ examples/use.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var expand = require('expand-args');
+var argv = require('minimist')(process.argv.slice(2));
+var base = require('base-methods');
+var store = require('base-store');
+var cli = require('..');
+var app = base()
+  .use(store('base-cli-test'))
+  .use(cli())
+
+app.store.on('use', function (key) {
+  console.log('[plugin]', key);
+});
+app.on('get', function (key, val) {
+  console.log('[get]', key, '=>', val);
+});
+
+app.cli.process(argv);
+// Try:
+//
+//  $ node examples/use.js --cwd=examples/plugins --use=a,b,c --get=a --get=b --get=c
+//
