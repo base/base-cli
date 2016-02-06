def9849399b6407767863115a02cd74c40580f0e
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-06 10:13:56 -0500

    lint

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var mocha = require('gulp-mocha');
 
 var lint = ['index.js'];
 
-gulp.task('coverage', function () {
+gulp.task('coverage', function() {
   return gulp.src(lint)
     .pipe(istanbul())
     .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['coverage'], function () {
+gulp.task('test', ['coverage'], function() {
   return gulp.src('test/*.js')
     .pipe(mocha({reporter: 'spec'}))
     .pipe(istanbul.writeReports())
@@ -23,7 +23,7 @@ gulp.task('test', ['coverage'], function () {
     }));
 });
 
-gulp.task('lint', function () {
+gulp.task('lint', function() {
   return gulp.src(lint.concat('test/*.js'))
     .pipe(eslint())
 });
