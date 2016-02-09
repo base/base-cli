b1cfc9de85403009880af144cd295c2b57f33eab
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-09 00:03:16 -0500

    update docs with verb

--- verbfile.js
+++ verbfile.js
@@ -9,6 +9,7 @@ module.exports = function(verb) {
     return through.obj(function(file, enc, next) {
       var str = file.contents.toString();
       str = str.replace(/^(#+ \[)\./gm, '$1--');
+      console.log('converted titles to flags');
       file.contents = new Buffer(str);
       next(null, file);
     });
