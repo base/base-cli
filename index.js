979e28e5acb50e7b0054e265c8c29a70860a22e3
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-19 17:59:56 -0500

    copyright

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /*!
  * base-cli <https://github.com/jonschlinkert/base-cli>
  *
- * Copyright (c) 2015, Jon Schlinkert.
+ * Copyright (c) 2015-2016, Jon Schlinkert.
  * Licensed under the MIT License.
  */
 
