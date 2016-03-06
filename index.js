b32facd66c32684c77fa63f8a466a6a7c16990c1
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-05 23:36:17 -0500

    usability improvements

--- index.js
+++ index.js
@@ -14,17 +14,25 @@ module.exports = function(options) {
   options = options || {};
 
   return function(app) {
-    this.use(utils.argv());
     this.use(utils.ask());
     this.use(utils.config.create('cli'));
+    this.use(utils.argv());
     argv(this, process.argv.slice(2));
     cli(this, options.keys || []);
+    update(this);
   };
 };
 
 function argv(app, args) {
-  define(app, '_argv', function() {
-    return app.argv(utils.minimist(args, {alias: utils.aliases}));
+  utils.define(app, '_argv', {
+    configurable: true,
+    enumerable: true,
+    set: function(val) {
+      utils.define(app, '_argv', val);
+    },
+    get: function() {
+      return app.argv(utils.minimist(args, {alias: utils.aliases}));
+    }
   });
 
   utils.define(app.argv, 'get', function(key) {
@@ -36,15 +44,13 @@ function argv(app, args) {
   });
 }
 
-function define(app, prop, val) {
-  utils.define(app, prop, {
-    set: function(v) {
-      utils.define(app, prop, v);
-    },
-    get: function fn() {
-      if (fn.val) return fn.val;
-      fn.val = val();
-      return fn.val;
+function update(app) {
+  var fn = app.cli.process;
+  utils.define(app.cli, 'process', function(args, cb) {
+    if (Array.isArray(args)) {
+      args = utils.minimist(args, {alias: utils.aliases});
     }
+    app._argv = args;
+    fn.apply(app.cli, arguments);
   });
 }
