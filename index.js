cae7afe28eaac629a3e9580d281d9a481536da46
jonschlinkert <jon.schlinkert@sellside.com>
2016-03-05 23:35:52 -0500

    adds `app._argv` with parsed arguments

--- index.js
+++ index.js
@@ -14,8 +14,37 @@ module.exports = function(options) {
   options = options || {};
 
   return function(app) {
+    this.use(utils.argv());
     this.use(utils.ask());
     this.use(utils.config.create('cli'));
+    argv(this, process.argv.slice(2));
     cli(this, options.keys || []);
   };
 };
+
+function argv(app, args) {
+  define(app, '_argv', function() {
+    return app.argv(utils.minimist(args, {alias: utils.aliases}));
+  });
+
+  utils.define(app.argv, 'get', function(key) {
+    return utils.get(app._argv, key);
+  });
+
+  utils.define(app.argv, 'has', function(key) {
+    return typeof app.argv.get(key) !== 'undefined';
+  });
+}
+
+function define(app, prop, val) {
+  utils.define(app, prop, {
+    set: function(v) {
+      utils.define(app, prop, v);
+    },
+    get: function fn() {
+      if (fn.val) return fn.val;
+      fn.val = val();
+      return fn.val;
+    }
+  });
+}
